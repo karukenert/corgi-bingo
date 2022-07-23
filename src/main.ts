96dc4db5672d166a30e15a6f1fd7df45c5f6209c
Kenert Karu <karukenert@gmail.com>
2022-07-23 15:41:34 +0300

    firebase init

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,23 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBKYMkyCvfULnC8-26iU3VllAlVwUgcP0",
+  authDomain: "corgi-bingo-805dd.firebaseapp.com",
+  projectId: "corgi-bingo-805dd",
+  storageBucket: "corgi-bingo-805dd.appspot.com",
+  messagingSenderId: "89276643112",
+  appId: "1:89276643112:web:b28363cec8e67eb0ea021d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createApp(App).mount('#app')
