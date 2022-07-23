33a57467a21f0dedab31b5be92398ebdb8e5c82f
Kenert Karu <karukenert@gmail.com>
2022-07-23 15:32:16 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+
+<template>
+  <div>
+    <h2>Tereee</h2>
+    <!-- <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />sys
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a> -->
+    <table>
+
+
+      <tbody>
+        
+              <th></th>
+              <td>Koer</td>
+
+      </tbody>
+    </table>
+  </div>
+
+</template>
+
+<style >
+body {
+    background-color: transparent !important;
+   background-image: url("https://i.pinimg.com/564x/7d/b2/86/7db2863c8654c7157824f7d42b67a169.jpg") !important;
+}
+
+</style>
